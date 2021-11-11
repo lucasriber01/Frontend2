17120169d1311e6949acaf115abdf4b7b321ce93
Jehan Lucas Vieira <lucasarago.df@gmail.com>
2021-11-10 22:02:56 -0300

    Final

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ function adicionar(){
 
 
 
-
-
-
     let lista = document.getElementById("lista").innerHTML;
-    lista += "<img src ="+imagem+" />"+"<h2>"+serie+"</h2>"+"<p>"+descricao+"</p>"+"<hr>";
-
+    lista += `       
+    <div class="card border-primary" style="width: 18rem;">
+    <img class="card-img-top" src="${imagem}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${serie}</h5>
+      <p class="card-text">${descricao}</p>
+      <a href="https://seriesbrasil.com.br/" class="btn btn-primary">Saiba Mais</a>
+    </div>
+  </div>
+`;
     document.getElementById("lista").innerHTML = lista ; 
 
-
-}
-function removeItem( lista ) {
-    document.getElementById('div_' + lista ).innerHTML = "";
 }
